0f43ac9b0fb3beb2085cedd4053a5e9c90ea26cf
iqtadars course <iqtadar.hassan888@gmail.com>
2020-05-01 22:01:17 +0500

    redundancy removes and delete button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-   def index
-    @articles = Article.all
-   end
-   
-   def new
-   	@article = Article.new
-   end
+    def index
+     @articles = Article.all
+    end
 
-   def edit
-      @article = Article.find(params[:id])
-   end
+    def new
+   	 @article = Article.new
+    end
+
+     def edit
+       @article = Article.find(params[:id])
+     end
 
    def create
    	#render plain: params[:articles].inspect
@@ -37,10 +38,19 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
    end
 
-   def delete
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article successfully deleted"
+    redirect_to articles_path
    end
 
+
       private
+
+    def set_article
+      @article = Article.find(params[:id]) 
+    end
    
     def article_params
         params.require(:article).permit(:title, :description)
