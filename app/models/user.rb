54ec74cb02c33d7cbef05c9f1598b1d8c5d92ca7
iqtadars course <iqtadar.hassan888@gmail.com>
2020-05-08 01:37:58 +0500

    add assc btwn user and dbug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase } #before saving email we convrt into downcase
   validates :username, presence: true, 
   uniqueness: { case_sensitive: false }, #doesnot matter if it is in capslock or  small... it should not be same
   length: { minimum: 3, maximum: 25 }
