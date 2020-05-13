e59999fd3a8f63fb370b1d5d1b8946466275e010
iqtadars course <iqtadar.hassan888@gmail.com>
2020-05-14 01:06:16 +0500

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save { self.email = email.downcase } #before saving email we convrt into downcase
   validates :username, presence: true, 
   uniqueness: { case_sensitive: false }, #doesnot matter if it is in capslock or  small... it should not be same
